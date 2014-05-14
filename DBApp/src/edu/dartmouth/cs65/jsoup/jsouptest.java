2bfaa3172d905db5e9a6095b29766e5932e2c711
evawxiao <oaixave@gmail.com>
2014-05-14 11:11:17 -0400

    Added jsoup shiz

---
+++ DBApp/src/edu/dartmouth/cs65/jsoup/jsouptest.java
@@ -0,0 +1,46 @@
+package edu.dartmouth.cs65.jsoup;
+
+import java.io.IOException;
+
+import org.jsoup.Connection;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+public class JSoupTest{
+	private Document webpage;
+	
+	public JSoupTest(){
+		try{
+			webpage = Jsoup.connect("https://dartmouth.managemyid.com/student/login.php").get();
+			
+		}
+		catch(IOException e){
+			System.out.println("Connection to ManageMyID failed");
+		}
+	}
+	
+	public void authenticate(String username, String password){
+		try{
+			Document doc = Jsoup
+					.connect("https://dartmouth.managemyid.com/student/login.php")
+					.data("txtUserName", username)
+					.data("txtPassword", password)
+					.method(Connection.Method.POST)
+					.get();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			System.out.println("Authentication failed.");
+		}
+	}
+	
+	public static void main(String[] args){
+		JSoupTest test = new JSoupTest();
+		System.out.println(test.webpage.data());
+		
+		//test.authenticate("eva.w.xiao@dartmouth.edu", "1dandelion");
+		
+	}
+}
