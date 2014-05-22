50720a4c99b63f16bcccfad49c89b4aade063f03
Janice Yip <janiceyip@janice-yips-mbp.wireless.dartmouth.edu>
2014-05-22 11:40:59 -0400

    globals

---
+++ DBApp/src/edu/dartmouth/cs65/Globals.java
@@ -0,0 +1,17 @@
+package edu.dartmouth.cs65;
+
+public class Globals {
+
+	public static final String DATE_FORMAT = "H:mm:ss MMM d yyyy";
+	
+	public static final String TABLE_NAME_ENTRIES = "entries"; 
+	public static final String KEY_ROWID = "id";
+	public static final String KEY_DATE_TIME = "date_time"; 
+	public static final String KEY_LOCATION = "location";
+	public static final String KEY_ACCOUNT_TYPE = "account_type"; 
+	public static final String KEY_TRANSACTION_TYPE = "transaction_type"; 
+	public static final String KEY_AMOUNT = "amount"; 
+	
+
+	
+}
