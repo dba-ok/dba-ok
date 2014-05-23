f7a677c7586c9c18f3c7d59d289ac117371265a1
evawxiao <oaixave@gmail.com>
2014-05-23 17:29:25 -0400

    Added info about opening hours in DiningLocations.java and edited LocationFragment.java

--- DBApp/src/edu/dartmouth/cs65/LocationsFragment.java
+++ DBApp/src/edu/dartmouth/cs65/LocationsFragment.java
@@ -1,6 +1,7 @@
 package edu.dartmouth.cs65;
 
-import edu.dartmouth.cs65.R;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,14 +11,18 @@ import android.view.ViewGroup;
 
 public class LocationsFragment extends Fragment {
 	
-	public LocationsFragment(){
-		
-	}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+    	Calendar now = Calendar.getInstance();
+        DiningLocations locations = new DiningLocations();
+        
+        ArrayList<DiningLocation> openLocations = locations.getOpenLocations(now.HOUR_OF_DAY, now.MINUTE, now.DAY_OF_WEEK);
+        ArrayList<DiningLocation> closedLocations = locations.getClosedLocations(now.HOUR_OF_DAY, now.MINUTE, now.DAY_OF_WEEK);
+        
+        
         return inflater.inflate(R.layout.locations, container, false);
     }
-    
+
 }
 
