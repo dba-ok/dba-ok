8604ca954857d9e0ef28c07e38ee8f5f5a4096ed
Janice Yip <janiceyip@janice-yips-mbp.wireless.dartmouth.edu>
2014-05-24 23:49:33 -0400

    changed graph

--- DBApp/src/edu/dartmouth/cs65/MyStatsFragment.java
+++ DBApp/src/edu/dartmouth/cs65/MyStatsFragment.java
@@ -81,17 +81,17 @@ public class MyStatsFragment extends Fragment {
         webSettings.setJavaScriptEnabled(true);
         webSettings.setBuiltInZoomControls(true);
 
-        FocoField = (EditText) getView().findViewById(R.id.foco);
-        CollisField = (EditText) getView().findViewById(R.id.collis);
-        HopField = (EditText) getView().findViewById(R.id.hop);
-        NovackField = (EditText) getView().findViewById(R.id.novack);
-        KafField = (EditText) getView().findViewById(R.id.kaf);
-        EWSnackBarField = (EditText) getView().findViewById(R.id.ewsnackbar);
-        CollisMarketField = (EditText) getView().findViewById(R.id.collismarket);
+//        FocoField = (EditText) getView().findViewById(R.id.foco);
+//        CollisField = (EditText) getView().findViewById(R.id.collis);
+//        HopField = (EditText) getView().findViewById(R.id.hop);
+//        NovackField = (EditText) getView().findViewById(R.id.novack);
+//        KafField = (EditText) getView().findViewById(R.id.kaf);
+//        EWSnackBarField = (EditText) getView().findViewById(R.id.ewsnackbar);
+//        CollisMarketField = (EditText) getView().findViewById(R.id.collismarket);
 
         initPieButton();
         initColumnButton(); 
-        initImeActionReload();
+//        initImeActionReload();
 
         loadChart("pie");
 	}
@@ -116,72 +116,85 @@ public class MyStatsFragment extends Fragment {
 	        });
 	}
 	
-	private void initImeActionReload() {
-		KafField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-	        @Override
-	        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-	            if (actionId == EditorInfo.IME_NULL) {
-	                loadChart("column");
-	                return true;
-	            }
-	
-	            return false;
-	        }
-	    });
-	}
+//	private void initImeActionReload() {
+//		KafField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//	        @Override
+//	        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//	            if (actionId == EditorInfo.IME_NULL) {
+//	                loadChart("column");
+//	                return true;
+//	            }
+//	
+//	            return false;
+//	        }
+//	    });
+//	}
 	
 	private void loadChart(String chart) {
-	    if (TextUtils.isEmpty(FocoField.getText())) {
-	    	FocoField.setText("0");
-	        return;
-	    }
-	
-	    if (TextUtils.isEmpty(CollisField.getText())) {
-	    	CollisField.setText("0");
-	        return;
-	    }
+//	    if (TextUtils.isEmpty(FocoField.getText())) {
+//	    	FocoField.setText("0");
+//	        return;
+//	    }
+//	
+//	    if (TextUtils.isEmpty(CollisField.getText())) {
+//	    	CollisField.setText("0");
+//	        return;
+//	    }
+//
+//	    if (TextUtils.isEmpty(HopField.getText())) {
+//	    	NovackField.setText("0");
+//	        return;
+//	    }
+//	    
+//	    if (TextUtils.isEmpty(NovackField.getText())) {
+//	    	NovackField.setText("0");
+//	        return;
+//	    }
+//	
+//	    if (TextUtils.isEmpty(KafField.getText())) {
+//	    	KafField.setText("0");
+//	        return;
+//	    }
+//	    
+//	    if (TextUtils.isEmpty(EWSnackBarField.getText())) {
+//	    	EWSnackBarField.setText("0");
+//	        return;
+//	    }
+//	
+//	    if (TextUtils.isEmpty(CollisMarketField.getText())) {
+//	    	CollisMarketField.setText("0");
+//	        return;
+//	    }
 
-	    if (TextUtils.isEmpty(HopField.getText())) {
-	    	NovackField.setText("0");
-	        return;
-	    }
 	    
-	    if (TextUtils.isEmpty(NovackField.getText())) {
-	    	NovackField.setText("0");
-	        return;
-	    }
-	
-	    if (TextUtils.isEmpty(KafField.getText())) {
-	    	KafField.setText("0");
-	        return;
-	    }
+//	    hideSoftKeyboard(FocoField);
+//	    hideSoftKeyboard(CollisField);
+//	    hideSoftKeyboard(HopField);
+//	    hideSoftKeyboard(NovackField);
+//	    hideSoftKeyboard(KafField);
+//	    hideSoftKeyboard(EWSnackBarField);
+//	    hideSoftKeyboard(CollisMarketField);
 	    
-	    if (TextUtils.isEmpty(EWSnackBarField.getText())) {
-	    	EWSnackBarField.setText("0");
-	        return;
-	    }
-	
-	    if (TextUtils.isEmpty(CollisMarketField.getText())) {
-	    	CollisMarketField.setText("0");
-	        return;
-	    }
-
+	    // put the dba by location usage here 
+	    int foco = 3; 
+	    int collis = 1; 
+	    int hop = 1; 
+	    int novack = 1; 
+	    int kaf = 1; 
+	    int ewsnackbar = 1; 
+	    int collismarket = 1; 
 	    
-	    hideSoftKeyboard(FocoField);
-	    hideSoftKeyboard(CollisField);
-	    hideSoftKeyboard(HopField);
-	    hideSoftKeyboard(NovackField);
-	    hideSoftKeyboard(KafField);
-	    hideSoftKeyboard(EWSnackBarField);
-	    hideSoftKeyboard(CollisMarketField);
-	    
-	    int foco = Integer.parseInt(FocoField.getText().toString());
-	    int collis = Integer.parseInt(CollisField.getText().toString());
-	    int hop = Integer.parseInt(HopField.getText().toString());
-	    int novack = Integer.parseInt(NovackField.getText().toString());
-	    int kaf = Integer.parseInt(KafField.getText().toString());
-	    int ewsnackbar = Integer.parseInt(EWSnackBarField.getText().toString());
-	    int collismarket = Integer.parseInt(EWSnackBarField.getText().toString());
+	    // put the dba by week usage here 
+	    int week1 = 100; 
+	    int week2 = 100; 
+	    int week3 = 100; 
+	    int week4 = 100; 
+	    int week5 = 100; 
+	    int week6 = 100; 
+	    int week7 = 100; 
+	    int week8 = 100; 
+	    int week9 = 100; 
+	    int week10 = 100; 
 	    
 	    String content = "null";
 	    try {
@@ -199,15 +212,21 @@ public class MyStatsFragment extends Fragment {
 	        Log.e(TAG, "An error occurred.", e);
 	    }
 	
-	    String formattedContent = String.format(content, foco, collis, hop, novack, kaf, ewsnackbar, collismarket);
-	    webView.loadDataWithBaseURL(ASSET_PATH, formattedContent, "text/html", "utf-8", null);
+	    if (chart.equals("pie")) {
+		    String formattedContent = String.format(content, foco, collis, hop, novack, kaf, ewsnackbar, collismarket);
+		    webView.loadDataWithBaseURL(ASSET_PATH, formattedContent, "text/html", "utf-8", null);	    	
+	    } else if (chart.equals("column")) {
+		    String formattedContent = String.format(content, week1, week2, week3, week4, week5, 
+		    		week6, week7, week8, week9, week10);
+		    webView.loadDataWithBaseURL(ASSET_PATH, formattedContent, "text/html", "utf-8", null);
+	    }
 	    webView.requestFocusFromTouch();
 	}
 	
-	private void hideSoftKeyboard(TextView v) {
-	    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
-	    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-	}
+//	private void hideSoftKeyboard(TextView v) {
+//	    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
+//	    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+//	}
 	
 /*	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
