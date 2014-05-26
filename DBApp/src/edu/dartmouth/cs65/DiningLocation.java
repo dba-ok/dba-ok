b1ad53ca92f80e7f649d01dd650e5050285199ae
evawxiao <oaixave@gmail.com>
2014-05-26 00:21:15 -0400

    commented DiningLocation, DiningLocations, and Time

--- DBApp/src/edu/dartmouth/cs65/DiningLocation.java
+++ DBApp/src/edu/dartmouth/cs65/DiningLocation.java
@@ -1,3 +1,13 @@
+/**
+ * DBA-OK
+ * 
+ * This file defines a class for different dining locations. In particular, it saves the name of the dining location and its hours
+ * of operation according to the day of the week. 
+ * 
+ * In addition, it has a useful 'isOpen' function that returns true or false (the location is or isn't open) based on a given time.
+ * 
+ */
+
 package edu.dartmouth.cs65;
 
 import java.util.HashMap;
@@ -19,7 +29,8 @@ public class DiningLocation {
 	}
 
 	/*
-	 * Adds opening hours for a given day(s)
+	 * Adds opening hours for a given time range. For example, addTime(0, 3, hours) would add the String[] hours
+	 * as values for keys Sunday - Wednesday. 
 	 */
 	public void addTime(int dayStart, int dayEnd, String[] hours) {
 		for (int i = dayStart; i <= dayEnd; i++) {
@@ -28,33 +39,20 @@ public class DiningLocation {
 	}
 
 	/*
-	 * Given a day and time, is this dining location open?
+	 * Given a day and time, is this dining location open? Return true
+	 * for open, false for closed. 
 	 */
-	public boolean isOpen(int currHour, int currMinute, int day) { //
+	public boolean isOpen(int currHour, int currMinute, int day) { 
 		String[] openHours = times.get(Globals.DAYS[day]);
-		// Log.d("TIMES","Got times for " + Globals.DAYS[day]);
 		Time current = new Time(currHour, currMinute);
 		boolean isOpen = false;
 
-		Log.d("CS65", "LOCATION NAME IS :" + name);
 		// Iterate through all opening times
 		for (String hours : openHours) {
 			String[] split_hours = hours.split("-");
 			Time start = convertStringToTime(split_hours[0]);
 			Time end = convertStringToTime(split_hours[1]);
 
-			Log.d("CS65", "Start " + start.getHour() + ":" + start.getMinute());
-			Log.d("CS65", "End" + end.getHour() + ":" + end.getMinute());
-
-			/*
-			 * Log.d("CS65", "current isPast start? " + current.isPast(start));
-			 * Log.d("CS65", "current is equal start? " +
-			 * current.isEqual(start)); Log.d("CS65", "current is before end? "
-			 * + current.isBefore(end));
-			 */
-			Log.d("CS65",
-					"Time is in interval?? "
-							+ start.isInInterval(start, end, current));
 			if (start.isInInterval(start, end, current)) {
 				isOpen = true;
 				break;
@@ -71,21 +69,27 @@ public class DiningLocation {
 		String AMPM;
 		int hour, minute;
 		Time time;
-
+		
+		//Get AM or PM
 		splitWhiteSpace = s.split(" ");
 		AMPM = splitWhiteSpace[1];
 
+		//Split hours and minutes
 		splitColon = splitWhiteSpace[0].split(":");
 		hour = Integer.parseInt(splitColon[0]);
-		if (splitColon[1].equals("00")) {
+		
+		if (splitColon[1].equals("00")) { 
 			minute = 0;
-		} else {
+		} 
+		else {
 			minute = Integer.parseInt(splitColon[1]);
 		}
+		
 		// Convert to military time
 		if (AMPM.equals("pm") && hour != 12) {
 			hour += MILITARY_CONVERSION;
-		} else if (AMPM.equals("am") && hour == 12) {
+		} 
+		else if (AMPM.equals("am") && hour == 12) {
 			hour = 0;
 		}
 
--- DBApp/src/edu/dartmouth/cs65/Time.java
+++ DBApp/src/edu/dartmouth/cs65/Time.java
@@ -1,3 +1,10 @@
+/**
+ * DBA-OK
+ * 
+ * This file defines the Time class. The Time class contains an hour and minute value and defines several functions that determine
+ * whether or not the time stored in the Time class is before, after, or at the same time as another Time object. It also
+ * determines whether or not the time stored in the Time object is between an interval of time. 
+ */
 package edu.dartmouth.cs65;
 
 public class Time {
@@ -15,14 +22,19 @@ public class Time {
 	public boolean isPast(Time other) {
 		if (other.getHour() < this.hour) {
 			return true;
-		} else if ((other.getHour() == this.hour)
+		} 
+		else if ((other.getHour() == this.hour)
 				&& (other.getMinute() < this.minute)) {
 			return true;
-		} else {
+		} 
+		else {
 			return false;
 		}
 	}
-
+	
+	/*
+	 * Returns true if this Time object is between the time of two others Time objects
+	 */
 	public boolean isInInterval(Time startInterval, Time endInterval, Time other) {
 		int startHour = startInterval.getHour();
 		int startMinute = startInterval.getMinute();
@@ -33,6 +45,8 @@ public class Time {
 		int intervalLength = 0;
 		int startInMinutes = startHour * 60 + startMinute;
 		int endInMinutes;
+		
+		//Calculate length of interval
 		if (endHour < startHour) {
 			endInMinutes = (endHour + 24) * 60 + endMinute;
 			intervalLength = endInMinutes - startInMinutes;
@@ -44,6 +58,10 @@ public class Time {
 		int otherHour = other.getHour();
 		int otherMinute = other.getMinute();
 		int otherInMinutes = 0;
+		
+		//Treat this hour as the same day, even if it's between midnight
+		//and 3AM (the value of Globals.LATEST_CLOSE_TIME) because that's how
+		//meal sqipes are treated
 		if(otherHour < Globals.LATEST_CLOSE_TIME){
 			otherInMinutes = (otherHour  + 24) * 60 + otherMinute;
 		}
@@ -52,9 +70,6 @@ public class Time {
 		}
 		
 		return (startInMinutes <= otherInMinutes && otherInMinutes <= endInMinutes);
-		
-		//int otherInMinutes = 
-
 	}
 	
 
